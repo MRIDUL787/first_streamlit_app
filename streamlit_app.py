50d69bac2f08c332bab07ff295f2d4233f2e0089
MRIDUL787 <114795628+MRIDUL787@users.noreply.github.com>
2022-10-02 12:24:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 import snowflake.connector
 import pandas
+import requests
+from urllib.error import URLError
+
 streamlit.title('My Moms New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -15,8 +18,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.write('Thanks for adding',add_my_fruit)
-mycur.execute("insert into fruit_load_list values ('from streamlit')")
+#streamlit.write('Thanks for adding',add_my_fruit)
+#mycur.execute("insert into fruit_load_list values ('from streamlit')")
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
@@ -25,7 +28,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
